369a95dd3e4be303fe6587a9905fea52198df141
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-02-28 18:08:42 -0300

    Adicionado material auxiliar para revisão

---
+++ Revisao/Exercicio.py
@@ -0,0 +1,8 @@
+baseDeDados = [
+    ['Fa fe fi fo Funk',	'Anira', 'Funk', 2019, '3:05'],
+    [ 'Sofrência de programar', 'Ada & Turing',	'Sertanejo', 1998, '2:58' ],
+    [ 'Rock’n Rolo', 'The Buns','Rock',	1984, '4:01' ],
+    [ 'Grifinoria Girls', 'Katy Potter', 'Pop',	2017, '2:25' ]
+]
+
+print (baseDeDados)
